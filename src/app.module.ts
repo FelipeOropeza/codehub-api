1b95cc59243abb2c9325d1e1412f635059f01cb0
FelipeOropeza <felipe2006.co@gmail.com>
2025-12-18 01:11:20 -0300

    feat: inicializa aplicação NestJS com Prisma e PostgreSQL
    - Configuração inicial do NestJS
    - Integração com Prisma e PostgreSQL
    - Criação dos modelos principais (User, Post, Comment, Like, Follow)
    - Estruturação dos módulos da aplicação
    - Configuração de testes e TypeScript

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
+import { LikesModule } from './likes/likes.module';
+import { FollowsModule } from './follows/follows.module';
+
+@Module({
+  imports: [AuthModule, UsersModule, PostsModule, CommentsModule, LikesModule, FollowsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
